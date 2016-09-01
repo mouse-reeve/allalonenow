ab406d3342c96f9ae62791c51d4b808a54345344
Mouse Reeve <mousereeve@riseup.net>
2016-09-01 14:46:00 -0700

    Test script for detecting images dimensions (WIP)

---
+++ scripts/sizefilter.py
@@ -0,0 +1,10 @@
+''' delete image files that are too small '''
+
+import cv2
+import numpy as np
+import os
+
+for filename in os.listdir('fermi/static/images/rooms/full/'):
+    image = cv2.imread(filename)
+    height, width, _ = image.shape
+    print height, width
