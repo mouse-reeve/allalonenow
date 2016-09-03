41420779f0d2ff1b8ece2e639d7d83c7ef599c01
Mouse Reeve <mousereeve@riseup.net>
2016-09-02 22:16:04 -0700

    Protractor style element

--- fermi/static/js/app.js
+++ fermi/static/js/app.js
@@ -2,7 +2,13 @@ $('#main').click(function () {
     setRoom();
 });
 
+var roomstack = [];
+
 var setRoom = function () {
+    var current = $('#room').attr('src');
+    if (current) {
+        roomstack.push(current);
+    }
     room = Math.floor(Math.random() * rooms.length);
     $('#room').attr('src', 'static/images/rooms/full/' + rooms[room]);
 };
