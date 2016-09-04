f7c37c3bc985bc3fbf45551c5d53aef8d609fcb7
Mouse Reeve <mousereeve@riseup.net>
2016-09-03 18:13:51 -0700

    Keybindings

--- fermi/static/js/app.js
+++ fermi/static/js/app.js
@@ -2,23 +2,47 @@ $('#main').click(function () {
     setRoom();
 });
 
+$('html').keydown(function(e){
+    if (e.which == 39) {
+        // right/down arrow keys
+        setRoom();
+    } else if (e.which == 37) {
+        backRoom();
+    }
+});
+
 var position = -1;
 
 var room = Math.floor(Math.random() * (rooms.length - 1));
 
 var setRoom = function () {
+    room++;
+    // ready the next photo
+    if (room > rooms.length - 1) {
+        room = 0;
+    }
+
     $('#room').attr('src', 'static/images/rooms/full/' + rooms[room][0]);
     $('#quote').css('color', rooms[room][1]);
 
     // move arrow
     position += 1;
     $('#arrow').css('transform', 'rotate(' + position + 'deg)');
+};
 
+var backRoom = function () {
     // ready the next photo
-    room++;
-    if (room > rooms.length - 1) {
-        room = 0;
+    room--;
+    if (room < 0) {
+        room = rooms.length - 1;
     }
+
+    $('#room').attr('src', 'static/images/rooms/full/' + rooms[room][0]);
+    $('#quote').css('color', rooms[room][1]);
+
+    // move arrow
+    position -= 1;
+    $('#arrow').css('transform', 'rotate(' + position + 'deg)');
 };
 
 setRoom();
