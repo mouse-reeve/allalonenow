948bdbda9a0afabe24c6b67c50d853ba24505b46
Mouse Reeve <mousereeve@riseup.net>
2016-09-03 17:55:54 -0700

    Don't queue past filenames
    no point now

--- fermi/static/js/app.js
+++ fermi/static/js/app.js
@@ -2,16 +2,11 @@ $('#main').click(function () {
     setRoom();
 });
 
-var roomstack = [];
 var position = -1;
 
 var room = Math.floor(Math.random() * (rooms.length - 1));
 
 var setRoom = function () {
-    var current = $('#room').attr('src');
-    if (current) {
-        roomstack.push(current);
-    }
     $('#room').attr('src', 'static/images/rooms/full/' + rooms[room][0]);
     $('#quote').css('color', rooms[room][1]);
 
