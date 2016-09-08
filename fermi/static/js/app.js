203596724676b24d20fbc9df78a72fab2f2435b3
Mouse Reeve <mousereeve@riseup.net>
2016-09-07 19:25:16 -0700

    Crossfade images
    Fixes #1

--- fermi/static/js/app.js
+++ fermi/static/js/app.js
@@ -20,13 +20,22 @@ var room = Math.floor(Math.random() * (rooms.length - 1));
 var muted = true;
 
 var setRoom = function () {
+    // delete zombies
+    $('.zombie').remove();
     room++;
     // ready the next photo
     if (room > rooms.length - 1) {
         room = 0;
     }
 
-    $('#room').attr('src', 'static/images/rooms/full/' + rooms[room][0]);
+    $('#room').clone().attr('src', 'static/images/rooms/full/' + rooms[room][0]).attr('id', 'newRoom').hide().css('opacity', 1).appendTo('.window');
+    $('#room').attr('id', 'oldRoom');
+    $('#newRoom').fadeIn(250);
+    $('#oldRoom').fadeOut(250, function () {
+        $('#oldRoom').remove();
+    });
+    $('#oldRoom').addClass('zombie');
+    $('#newRoom').attr('id', 'room');
     $('#quote').css('color', rooms[room][1]);
 
     // move arrow
